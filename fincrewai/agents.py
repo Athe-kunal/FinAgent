61c2f8dba5dbd062e5a8fd066f4a5fd8d62b3740
Athe-kunal <athekunal@gmail.com>
2024-05-23 18:45:27 -0400

    Marker for extraction

--- fincrewai/agents.py
+++ fincrewai/agents.py
@@ -19,6 +19,7 @@ def get_agents(sec_tools_list,earnings_calls_tools_list):
         role = "Earnings Call transcripts Financial Data Analyst",
         goal = "Answer the question in detail by first using the relevant tool from the relevant quarter to get relevant documents. ",
         backstory = "With access to earnings call transcripts quarterly data you can get relevant documents from these documents to answer the question. "
+                    "The earnings call transcripts has data about what the executive team of the company said about the company current financials and future directions. "
                     "Don't answer the questions which is outside the scope of the relevant documents. "
                     "Include all the relevant numerical figures in your answer.",
         verbose=True,
