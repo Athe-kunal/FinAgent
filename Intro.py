dcb25b9f11a16414380c61c9b70a71f97d2338b2
Athe-kunal <athekunal@gmail.com>
2024-05-25 17:46:40 -0400

    streamlit app

---
+++ Intro.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from datetime import datetime
+from main import get_crewai_agent
+curr_year = datetime.now().year
+ticker = st.text_input(label="Ticker")
+year = st.text_input(label="Year")
+
+if year != "":
+    int_year = int(float(year))
+submit_button = st.button(label="Submit")
+if ticker != "" and year != "" and submit_button:
+    if curr_year == int_year:
+        curr_year_bool = True
+    else:
+        curr_year_bool = False
+    finance_docs_crewai = get_crewai_agent(ticker,year)
+    st.session_state["ticker"] = ticker
+    st.session_state["year"] = str(year)
+    st.session_state["finance_docs_crew"] = finance_docs_crewai 
+    st.write("Created Database")
---
+++ pages/Chat.py
@@ -0,0 +1,48 @@
+from streamlit_feedback import streamlit_feedback
+import streamlit as st
+from dotenv import load_dotenv,find_dotenv
+import openai
+import os
+import re
+
+load_dotenv(find_dotenv(),override=True)
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
+ticker = st.session_state["ticker"]
+year = st.session_state["year"]
+financial_docs_crew = st.session_state["finance_docs_crew"]
+st.title(f"{ticker}-{year}")
+
+
+def generate_response(input_text):
+    res = financial_docs_crew.kickoff({"question":input_text})
+    return res
+
+
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Hi, how can I help you?"}
+    ]
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+if prompt := st.chat_input():
+    # Display user message in chat message container
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
+
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Answering..."):
+            docs = generate_response(prompt)
+            docs = re.sub(r"\$", r"\\$", docs)
+            st.write(docs)
+            feedback = streamlit_feedback(
+                feedback_type="thumbs",
+                optional_text_label="Please describe the feedback in detail",
+            )
+    message = {"role": "assistant", "content": docs}
+    st.session_state.messages.append(message)
